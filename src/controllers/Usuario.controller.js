931bd46f1f24abfd966d6dacae94aabd01ddce49
Et3rnauta <guidocanevello@hotmail.com>
2022-11-01 21:02:48 -0300

    Permitir modificar la contraseña

--- src/controllers/Usuario.controller.js
+++ src/controllers/Usuario.controller.js
@@ -134,3 +134,28 @@ module.exports.usuarios_get_by_name_with_password = async function (nombreCuenta
 
     return query;
 }
+
+module.exports.usuarios_get_with_password = async function (id) {
+    const query = await Usuario.findById(id).select('+password').exec()
+        .catch((error) => {
+            if (error.name === "CastError") {
+                throw {
+                    number: 400,
+                    content: "Id incorrecto",
+                }
+            } else {
+                throw {
+                    content: error,
+                }
+            }
+        });
+
+    if (query === null) {
+        throw {
+            number: 404,
+            content: "No se encuentra el Usuario",
+        }
+    }
+
+    return query;
+}
