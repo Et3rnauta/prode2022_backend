e18d5127d682ebc1c6fe0e165c987b772f4c538e
Et3rnauta <guidocanevello@hotmail.com>
2022-11-28 21:19:04 -0300

    Se agrega modelo de Jugadores y rutas para editarlos

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,12 +3,14 @@ const userRouter = require('./Usuario.routes');
 const prediccionRouter = require('./Prediccion.routes');
 const equipoRouter = require('./Equipo.routes');
 const partidoRouter = require('./Partido.routes');
+const jugadorRouter = require('./Jugador.routes');
 const masterRoutes = require('./Master.routes');
 
 app.use(userRouter);
 app.use(prediccionRouter);
 app.use(equipoRouter);
 app.use(partidoRouter);
+app.use(jugadorRouter);
 app.use(masterRoutes);
 
 module.exports = app;
